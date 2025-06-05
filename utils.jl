34fb82a718bfc24d1697e4b078e71329f5cf8acc
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-06-05 18:31:53 +0200

    move showcase func

--- utils.jl
+++ utils.jl
@@ -5,6 +5,7 @@ const CONF = TOML.parsefile("./config.toml")
 
 include("./utils/cover.jl")
 include("./utils/sidebar.jl")
+include("./utils/showcase.jl")
 
 function hfun_navigation()
   io  = IOBuffer()
@@ -26,52 +27,4 @@ end
 
 @lx function br(; n=1)
   repeat("<br>", n)  |> html
-end
-
-@lx function showcase(; ref="", byyear=false)
-
-  file = joinpath(ROOT, "_assets", "items", ref)
-
-  toml = TOML.parsefile(file)
-
-  items = [i[2] for i in toml] |> (x -> sort(x, by=(e->e["date"]), rev=true))
-
-  isempty(items) && return ""
-
-  io      = IOBuffer()
-  curyear = year(items[1]["date"])
-
-  byyear && write(io, "~~~\n<h2>$(curyear)</h2>\n~~~\n")
-
-  for item in items
-    if byyear && year(item["date"]) < curyear
-      curyear = year(item["date"])
-      write(io, "~~~\n<h2>$(curyear)</h2>\n~~~\n")
-    end
-
-    title  = item["title"]
-    date   = item["date"]
-    bullet = "~~~<p>"
-
-    haskey(item, "url") ?
-    bullet *= "<a href=\"$(item["url"])\"> $(title) </a><br>" :
-    bullet *= "$(title)<br>"
-
-    haskey(item, "authors") ? 
-    bullet *= "<small>$(item["authors"])</small><br>" : nothing
-
-    haskey(item, "venue") ? 
-    bullet *= "<small><i>$(item["venue"])</i></small>" : nothing
-
-    haskey(item, "journal") ? 
-    bullet *= "<small><i>$(item["journal"])</i></small>" : nothing
-
-    haskey(item, "url") ? url = item["url"] : url = ""
-
-    bullet *= "</p>~~~"
-
-    write(io, "- $(bullet)\n")
-  end
-
-  take!(io) |> String 
 end
---
+++ utils/showcase.jl
@@ -0,0 +1,48 @@
+
+@lx function showcase(; ref="", byyear=false)
+
+  file = joinpath(ROOT, "_assets", "items", ref)
+
+  toml = TOML.parsefile(file)
+
+  items = [i[2] for i in toml] |> (x -> sort(x, by=(e->e["date"]), rev=true))
+
+  isempty(items) && return ""
+
+  io      = IOBuffer()
+  curyear = year(items[1]["date"])
+
+  byyear && write(io, "~~~\n<h2>$(curyear)</h2>\n~~~\n")
+
+  for item in items
+    if byyear && year(item["date"]) < curyear
+      curyear = year(item["date"])
+      write(io, "~~~\n<h2>$(curyear)</h2>\n~~~\n")
+    end
+
+    title  = item["title"]
+    date   = item["date"]
+    bullet = "~~~<p>"
+
+    haskey(item, "url") ?
+    bullet *= "<a href=\"$(item["url"])\"> $(title) </a><br>" :
+    bullet *= "$(title)<br>"
+
+    haskey(item, "authors") ? 
+    bullet *= "<small>$(item["authors"])</small><br>" : nothing
+
+    haskey(item, "venue") ? 
+    bullet *= "<small><i>$(item["venue"])</i></small>" : nothing
+
+    haskey(item, "journal") ? 
+    bullet *= "<small><i>$(item["journal"])</i></small>" : nothing
+
+    haskey(item, "url") ? url = item["url"] : url = ""
+
+    bullet *= "</p>~~~"
+
+    write(io, "- $(bullet)\n")
+  end
+
+  take!(io) |> String 
+end
