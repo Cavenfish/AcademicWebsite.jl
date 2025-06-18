17ba929a992a33fc3cbc2e187f024dd5adbcb670
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-06-18 09:30:45 +0200

    minor style edits

--- utils.jl
+++ utils.jl
@@ -8,6 +8,7 @@ include("./utils/sidebar.jl")
 include("./utils/showcase.jl")
 include("./utils/cards.jl")
 include("./utils/nav.jl")
+include("./utils/software.jl")
 
 @lx function br(; n=1)
   repeat("<br>", n)  |> html
