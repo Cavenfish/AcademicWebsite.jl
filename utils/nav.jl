19d75bffb7b9280883610094caef9fd4a5f70709
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-06-19 23:42:18 +0200

    make nav word with subdirs in pages

--- utils/nav.jl
+++ utils/nav.jl
@@ -20,5 +20,24 @@ function hfun_navigation()
     )
   end
 
+  for sub in inf[2]
+    ind = joinpath(inf[1], sub, "index.md")
+
+    if isfile(ind)
+      ref  = joinpath("pages", sub)
+      tmp  = joinpath(ref, "index")
+      name = pagevar(tmp, "name")
+
+      write(
+        io, 
+        """
+        <li>
+          <a href="/$(ref)">$(name)</a>
+        </li>\n
+        """
+      )
+    end
+  end
+  
   take!(io) |> String
 end
