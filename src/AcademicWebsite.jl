20dbacab2aecfd7644baba2410e64799de66e8e1
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-08-12 08:33:26 +0200

    make site init

--- src/AcademicWebsite.jl
+++ src/AcademicWebsite.jl
@@ -1,5 +1,12 @@
+"""
+AcademicWebsite.jl
+
+A static site template for academics.
+"""
 module AcademicWebsite
 
+  export init_site
 
+  include("./init_site.jl")
 
 end
---
+++ src/init_site.jl
@@ -0,0 +1,31 @@
+
+function init_site(topdir::String)
+
+  if topdir == "." || topdir == "./"
+    topdir = pwd()
+  elseif topdir[1] == '.' && topdir[2] == '/'
+    tmp = replace(topdir, "./" => "")
+    topdir = joinpath(pwd(), tmp)
+  else
+    topdir = joinpath(pwd(), topdir)
+  end
+
+  if !isdir(topdir)
+    @info "Making new directory at:\n  $(topdir)"
+    mkdir(topdir)
+  end
+
+  user_items_dir = joinpath(@__DIR__, "user_items")
+
+  for item in readdir(user_items_dir)
+    src = joinpath(user_items_dir, item)
+    dst = joinpath(topdir, item)
+    
+    @info "Copying over item.\n  $(tmp)"
+
+    cp(src, dst; force=true)
+  end
+
+  joinpath(topdir, "pages") |> mkdir
+  
+end
